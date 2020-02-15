49848088ddd458c4f486064a55eb3da38eff8d11
chosenbreed37 <ericl@digiterre.com>
2020-02-15 21:31:01 +0000

    Added solution for Triangle problem

---
+++ Sorting/Triangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Sorting
+{
+    class Triangle
+    {
+        public static bool IsTriangular(int pth, int qth, int rth)
+        {
+            return pth + qth > rth
+                && qth + rth > pth
+                && rth + pth > qth;
+        }
+
+        public static int ExecuteBruteForce(int[] A)
+        {
+            for (var p = 0; p < A.Length - 3; p++)
+            {
+                for (var q = p + 1; q < A.Length - 2; q++)
+                {
+                    for (var r = p + 2; r < A.Length - 1; r++)
+                    {
+                        if (IsTriangular(p, q, r))
+                        {
+                            return 1;
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public static int Execute(int[] A)
+        {
+            Array.Sort(A);
+
+            for (var n = 0; n < A.Length - 3; n++)
+            {
+                if (IsTriangular(A[n], A[n + 1], A[n + 2]))
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ Sorting/TriangleTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Sorting
+{
+    class TriangleTest
+    {
+        [Test]
+        public void Sample1()
+        {
+            var A = new int[] { 10, 2, 5, 1, 8, 20 };
+            var expected = 1;
+            var actual = Triangle.Execute(A);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Sample2()
+        {
+            var A = new int[] { 10, 50, 5, 1 };
+            var expected = 0;
+            var actual = Triangle.Execute(A);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void LargeSample1()
+        {
+            var generator = new Random();
+            var A = Enumerable.Range(0, 100000).Select(x => generator.Next(Int32.MinValue, Int32.MaxValue)).ToArray();
+            var expected = 1;
+            var actual = Triangle.Execute(A);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
