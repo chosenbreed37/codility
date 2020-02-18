75f0a5ee8137e1ad9a89211a14f174f7dd27ba78
chosenbreed37 <ericl@digiterre.com>
2020-02-18 23:22:18 +0000

    Added solution for intersecting discs problem.  Score 68%.

---
+++ Sorting/IntersectingDiscs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Sorting
+{
+    public class IntersectingDiscs
+    {
+        public static int Execute(int[] A)
+        {
+            var intersections = 0;
+            for (var i = 0; i < A.Length; i++)
+                for (var j = i + 1; j < A.Length; j++)
+                {
+                    if (i == j) continue;
+                    if (((long)i + A[i]) >= ((long)j - A[j])) intersections++;
+                    if (intersections > 10000000) return -1;
+                }
+            return intersections;
+        }
+    }
+}
---
+++ Sorting/IntersectingDiscsTest.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Sorting
+{
+    class IntersectingDiscsTests
+    {
+        [Test]
+        public void Sample()
+        {
+            var A = new int[] { 1, 5, 2, 1, 4, 0 };
+            var expected = 11;
+            var actual = IntersectingDiscs.Execute(A);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Sample2()
+        {
+            var A = new int[] { 1, 1, 1 };
+            var expected = 3;
+            var actual = IntersectingDiscs.Execute(A);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Sample3()
+        {
+            var A = new int[] { 1, 2147483647, 0 };
+            var expected = 2;
+            var actual = IntersectingDiscs.Execute(A);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void With1000()
+        {
+            var generator = new Random();
+            var A = Enumerable.Range(0, 1000)
+                .Select(_ => generator.Next(0, Int32.MaxValue))
+                .ToArray();
+            var expected = 11;
+            var actual = IntersectingDiscs.Execute(A);
+        }
+
+        [Test]
+        public void With10000()
+        {
+            var generator = new Random();
+            var A = Enumerable.Range(0, 10000)
+                .Select(_ => generator.Next(0, Int32.MaxValue))
+                .ToArray();
+            var expected = 11;
+            var actual = IntersectingDiscs.Execute(A);
+            //Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void With100000()
+        {
+            var generator = new Random();
+            var A = Enumerable.Range(0, 100000)
+                .Select(_ => generator.Next(0, Int32.MaxValue))
+                .ToArray();
+            var expected = 11;
+            var actual = IntersectingDiscs.Execute(A);
+            //Assert.AreEqual(expected, actual);
+        }
+    }
+}
